0fcf07819fd1d218fefb0b07b3949cf016a30ada
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-15 18:25:05 +0530

    P2 Done

--- Surveillance-Bureau/src/Pages/Landing Page/P2/P2.jsx
+++ Surveillance-Bureau/src/Pages/Landing Page/P2/P2.jsx
@@ -1,8 +1,31 @@
 import "./P2.scss"
+import Header from "../../../components/Header/Header"
+import Spline from '@splinetool/react-spline';
+
+import Cop from "../../../assets/Cop.png"
+
 const P2 = () => {
   return (
     <div className="P2">
-        P2
+      <div className="Top">
+        <Spline scene="https://prod.spline.design/uXYZOyWocYfl63Va/scene.splinecode" />
+        <Header />
+      </div>
+      <div className="Main">
+        <div className="Left">
+          <div className="Text">
+            <h1>Revolutionizing law enforcement and Integrity of National security !</h1>
+          </div>
+        </div>
+        <div className="Right">
+          <div className="Cop">
+            <img src={Cop} alt="cop" />
+          </div>
+        </div>
+      </div>
+      <div className="Bottom">
+        <button>GET STARTED</button>
+      </div>
     </div>
   )
 }
