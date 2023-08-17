6b9c70de1542e2459c28a83f40f25b6634948184
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-18 00:04:05 +0530

    Dashboard Initialized

--- Surveillance-Bureau/src/Pages/Dashboard/Dashboard.jsx
+++ Surveillance-Bureau/src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,52 @@
 import "./Dashboard.scss"
+import DashboardHeader from "./Dashboard Header/DashboardHeader"
+
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+import HistoryIcon from '@mui/icons-material/History';
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="Dashboard">
+      <DashboardHeader />
+      <main className="Main">
+        <div className="Cards">
+          <div className="Card IdentifyCriminal">
+            <div className="Top">
+              <FingerprintIcon className="Icon" />
+            </div>
+            <div className="Bottom">
+              Identify Criminal
+            </div>
+          </div>
+          <div className="Card RegisterCriminal">
+            <div className="Top">
+              <PersonAddAlt1OutlinedIcon className="Icon" />
+            </div>
+            <div className="Bottom">
+              Register Criminal
+            </div>
+          </div>
+          <div className="Card UpdateCriminal">
+            <div className="Top">
+              <EditNoteOutlinedIcon className="Icon" />
+            </div>
+            <div className="Bottom">
+              Update Criminal
+            </div>
+          </div>
+          <div className="Card ViewHistory">
+            <div className="Top">
+              <HistoryIcon className="Icon" />
+            </div>
+            <div className="Bottom">
+              View History
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
--- Surveillance-Bureau/src/Pages/Home/Home.jsx
+++ Surveillance-Bureau/src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import "./Home.scss"
 import Stations from "./Stations/Stations"
+import Dashboard from "../../Pages/Dashboard/Dashboard"
 const Home = () => {
   return (
     <div className="Home">
-      <Stations />
+    <Dashboard />
     </div>
   )
 }
