6f9eebbb0f6abf5d0f202c7f4016a507c2a1f4e9
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-26 22:14:16 +0530

    Dashboard Done

--- Surveillance-Bureau/src/Pages/Dashboard/Dashboard Header/DashboardHeader.jsx
+++ Surveillance-Bureau/src/Pages/Dashboard/Dashboard Header/DashboardHeader.jsx
@@ -1,32 +1,34 @@
-import "./DashboardHeader.scss"
-import MenuIcon from '@mui/icons-material/Menu';
-import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
-import MarkUnreadChatAltOutlinedIcon from '@mui/icons-material/MarkUnreadChatAltOutlined';
-import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import "./DashboardHeader.scss";
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
+import MarkUnreadChatAltOutlinedIcon from "@mui/icons-material/MarkUnreadChatAltOutlined";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import TextScroller from "./TextScroller";
+import SidebarIcon from "../SidebarIcon/SidebarIcon";
+import { useState } from "react";
 
-const DashboardHeader = () => {
-    return (
-        <div className="DashboardHeader">
-            <div className="Sidebar">
-                <MenuIcon className="Icon" />
-            </div>
-            <div className="Heading">
-                <div className="Notice">
-                    <TextScroller text="Notice: This is a demo website. Please do not enter any sensitive information." />
-                </div>
-                <div className="Location">
-                    <PlaceOutlinedIcon className="Icon" />
-                </div>
-            </div>
-            <div className="Icons">
-                <MarkUnreadChatAltOutlinedIcon className="Icon" />
-                <NotificationsActiveOutlinedIcon className="Icon" />
-                <AccountCircleOutlinedIcon className="Icon" />
-            </div>
+const DashboardHeader = ({ isSidebarOpen, toggleSidebar }) => {
+
+  return (
+    <div className="DashboardHeader">
+      <div className="SidebarX">
+      <SidebarIcon isOpen={isSidebarOpen} onClick={toggleSidebar} style={{ zIndex: isSidebarOpen ? 1001 : 'auto' }} />
+      </div>
+      <div className="Heading">
+        <div className="Notice">
+          <TextScroller text="Notice: This is a demo website. Please do not enter any sensitive information." />
         </div>
-    )
-}
+        <div className="Location">
+          <PlaceOutlinedIcon className="Icon" />
+        </div>
+      </div>
+      <div className="Icons">
+        <MarkUnreadChatAltOutlinedIcon className="Icon" />
+        <NotificationsActiveOutlinedIcon className="Icon" />
+        <AccountCircleOutlinedIcon className="Icon" />
+      </div>
+    </div>
+  );
+};
 
-export default DashboardHeader
+export default DashboardHeader;
